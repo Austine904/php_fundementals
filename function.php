10230ba5670a9137c51cd6b0fdaa79efdd99474e
Austine904 <ochiengaustine89@gmail.com>
2025-03-03 02:49:02 +0300

    Function

---
+++ function.php
@@ -0,0 +1,30 @@
+<?php
+    /*function happy_birthday($first_name, $age){
+        echo "happy birthday dear {$first_name}.. <br>";
+        echo "happy birthday to you!..<br>";
+        echo "happy birthday dear {$first_name}..<br>";
+        echo "how old are you now {$age}..?<br><br>";
+    }
+
+    happy_birthday("spongebob", 30);
+    happy_birthday("Patrick", 35);
+    happy_birthday("Ochieng", 60);
+    happy_birthday("waweru", 90);
+
+
+function is_even($number){
+    $result = $number % 2;
+    return $result;
+}
+    echo is_even(45);
+*/
+
+function hypotenuse($a, $b){
+
+    $c = sqrt($a**2 + $b**2);
+    return $c;
+}
+echo hypotenuse(3, 4)
+
+
+?>

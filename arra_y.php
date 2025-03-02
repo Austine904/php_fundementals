a3ac4571cc69ab651855b288b4fed781585664c5
Austine904 <ochiengaustine89@gmail.com>
2025-03-02 23:00:46 +0300

    Arrays

---
+++ arra_y.php
@@ -0,0 +1,15 @@
+<?php
+$foods = array("apple", "orange", "banana", "coconut");
+ 
+//$foods[0] = "pineaple";
+//array_push($foods, "mango", "oranges");
+//array_pop($foods);
+//array_shift($foods);
+//$foods = array_reverse($foods);
+//echo count($foods);
+
+
+foreach($foods as $food){
+    echo $food . "<br>";
+ }
+?>

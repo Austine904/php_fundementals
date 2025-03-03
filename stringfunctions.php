0f9c269c80917219f3f803dd4eb4757d3876ca7f
Austine904 <ochiengaustine89@gmail.com>
2025-03-03 03:24:50 +0300

    string functions part one

---
+++ stringfunctions.php
@@ -0,0 +1,25 @@
+<?php
+$username = array ("BASTOTO", "FROM", "OCHI");
+$phone = "123-456-789";
+
+
+//$username = strtolower($username);
+//$username = str_pad($username, 20, "2");
+//$phone = str_replace("-", "", $phone);
+//$username = strrev($username);
+//$username = str_shuffle($username);
+//$equals = strcmp($username, "BASTOTO OCHI");
+//$count = strlen($phone);
+//$index = strpos($username, " ");
+//$firstname = substr($username,0, 3);
+
+
+
+
+
+
+echo"$name<br>";
+echo "$phone";
+
+
+?>
